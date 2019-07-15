a463335107a2adc9dc0f1a1d434ba0caf241853f
William Candillon <wcandillon@gmail.com>
2019-07-15 20:52:54 +0200

    💚

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
 export { default as Thumbnail } from "./Thumbnail";
-export { default as Card } from "./Card";
+export { default as Card, cards } from "./Card";
 export { default as StyleGuide } from "./StyleGuide";
 export { default as LoadAssets } from "./LoadAssets";

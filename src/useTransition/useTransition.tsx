ba0cb626d74c35a64b3b5fff311d22dc3a74e40e
William Candillon <wcandillon@gmail.com>
2019-07-18 13:56:35 +0200

    ⚙️

--- src/useTransition/useTransition.tsx
+++ src/useTransition/useTransition.tsx
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Dimensions } from "react-native";
 import Animated, { Easing } from "react-native-reanimated";
-import { useTransition, bInterpolate } from "react-native-redash";
+import {
+  useTransition,
+  bInterpolate,
+  transformOrigin
+} from "react-native-redash";
 
 import { Card, Button, StyleGuide, cards } from "../components";
 
-const { multiply } = Animated;
+const { multiply, interpolate } = Animated;
+const { width } = Dimensions.get("window");
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -29,20 +34,42 @@ export default () => {
     400,
     Easing.inOut(Easing.ease)
   );
-  const rotate = bInterpolate(transitionVal, 0, Math.PI / 6);
+
   return (
     <View style={styles.container}>
-      {cards.map((card, index) => (
-        <Animated.View
-          key={card.id}
-          style={[
-            styles.overlay,
-            { transform: [{ rotate: multiply(rotate, index) }] }
-          ]}
-        >
-          <Card {...{ card }} />
-        </Animated.View>
-      ))}
+      {cards.map((card, index) => {
+        const rotation = interpolate(index, {
+          inputRange: [0, 1, 2],
+          outputRange: [-1, 0, 1]
+        });
+        const rotate = multiply(
+          rotation,
+          bInterpolate(transitionVal, 0, Math.PI / 6)
+        );
+        return (
+          <Animated.View
+            key={card.id}
+            style={[
+              styles.overlay,
+              {
+                transform: [
+                  {
+                    translateX: -width / 2
+                  },
+                  {
+                    rotate
+                  },
+                  {
+                    translateX: width / 2
+                  }
+                ]
+              }
+            ]}
+          >
+            <Card {...{ card }} />
+          </Animated.View>
+        );
+      })}
       <Button
         label={toggled ? "Reset" : "Start"}
         primary

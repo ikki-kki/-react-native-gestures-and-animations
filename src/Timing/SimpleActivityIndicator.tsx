63f94f10254cc554a085ace3d9e3e5b117502153
William Candillon <wcandillon@gmail.com>
2019-07-20 15:31:27 +0200

    ⚙️

--- src/Timing/SimpleActivityIndicator.tsx
+++ src/Timing/SimpleActivityIndicator.tsx
@@ -1,11 +1,56 @@
 import * as React from "react";
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 import Animated from "react-native-reanimated";
+import { StyleGuide } from "../components";
 
 interface SimpleActivityIndicatorProps {
   progress: Animated.Value<number>;
 }
 
+const { interpolate, Extrapolate } = Animated;
+const size = 32;
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-evenly"
+  },
+  bubble: {
+    width: size,
+    height: size,
+    borderRadius: size / 2,
+    backgroundColor: StyleGuide.palette.primary
+  }
+});
+
 export default ({ progress }: SimpleActivityIndicatorProps) => {
-  return <View />;
+  const bubbles = 3;
+  const delta = 1 / bubbles;
+  return (
+    <View style={styles.container}>
+      {new Array(bubbles)
+        .fill(0)
+        .map((_, i) => i)
+        .map(i => {
+          const start = i * delta;
+          const end = start + delta;
+          const opacity = interpolate(progress, {
+            inputRange: [start, end],
+            outputRange: [0.5, 1],
+            extrapolate: Extrapolate.CLAMP
+          });
+          const scale = interpolate(progress, {
+            inputRange: [start, end],
+            outputRange: [1, 1.5],
+            extrapolate: Extrapolate.CLAMP
+          });
+          return (
+            <Animated.View
+              key={i}
+              style={[styles.bubble, { opacity, transform: [{ scale }] }]}
+            />
+          );
+        })}
+    </View>
+  );
 };
--- src/Timing/Timing.tsx
+++ src/Timing/Timing.tsx
@@ -1,20 +1,66 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
-import Animated from "react-native-reanimated";
+import Animated, { Easing } from "react-native-reanimated";
 
 import SimpleActivityIndicator from "./SimpleActivityIndicator";
+import { StyleGuide } from "../components";
 
-const { Value } = Animated;
+const {
+  Clock,
+  Value,
+  useCode,
+  block,
+  set,
+  cond,
+  not,
+  clockRunning,
+  startClock,
+  timing
+} = Animated;
+
+const runLoop = (
+  clock: Animated.Clock,
+  duration: Animated.Adaptable<number>,
+  easing: Animated.EasingFunction
+) => {
+  const state = {
+    finished: new Value(0),
+    position: new Value(0),
+    time: new Value(0),
+    frameTime: new Value(0)
+  };
+  const config = {
+    toValue: new Value(1),
+    duration,
+    easing
+  };
+
+  return block([
+    cond(not(clockRunning(clock)), startClock(clock)),
+    timing(clock, state, config),
+    cond(state.finished, [
+      set(state.finished, 0),
+      set(state.time, 0),
+      set(state.frameTime, 0),
+      set(config.toValue, cond(config.toValue, 0, 1))
+    ]),
+    state.position
+  ]);
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
+    backgroundColor: StyleGuide.palette.background
   }
 });
 
 export default () => {
+  const clock = new Clock();
   const progress = new Value(0);
+  useCode(set(progress, runLoop(clock, 1000, Easing.inOut(Easing.ease))), []);
   return (
     <View style={styles.container}>
       <SimpleActivityIndicator {...{ progress }} />

a21bbbd3ffffac5930b563be00771af33d3908fa
William Candillon <wcandillon@gmail.com>
2019-07-24 10:05:23 +0200

    ⚙️

--- src/Timing/SimpleActivityIndicator.tsx
+++ src/Timing/SimpleActivityIndicator.tsx
@@ -12,6 +12,11 @@ const width = wWidth * 0.8;
 const { interpolate, Extrapolate } = Animated;
 const size = 32;
 const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
   container: {
     height: width,
     width,
@@ -35,27 +40,29 @@ export default ({ progress }: SimpleActivityIndicatorProps) => {
   const bubbles = [0, 1, 2];
   const delta = 1 / bubbles.length;
   return (
-    <View style={styles.container}>
-      {bubbles.map(i => {
-        const start = i * delta;
-        const end = start + delta;
-        const opacity = interpolate(progress, {
-          inputRange: [start, end],
-          outputRange: [0.5, 1],
-          extrapolate: Extrapolate.CLAMP
-        });
-        const scale = interpolate(progress, {
-          inputRange: [start, end],
-          outputRange: [1, 1.5],
-          extrapolate: Extrapolate.CLAMP
-        });
-        return (
-          <Animated.View
-            key={i}
-            style={[styles.bubble, { opacity, transform: [{ scale }] }]}
-          />
-        );
-      })}
+    <View style={styles.root}>
+      <View style={styles.container}>
+        {bubbles.map(i => {
+          const start = i * delta;
+          const end = start + delta;
+          const opacity = interpolate(progress, {
+            inputRange: [start, end],
+            outputRange: [0.5, 1],
+            extrapolate: Extrapolate.CLAMP
+          });
+          const scale = interpolate(progress, {
+            inputRange: [start, end],
+            outputRange: [1, 1.5],
+            extrapolate: Extrapolate.CLAMP
+          });
+          return (
+            <Animated.View
+              key={i}
+              style={[styles.bubble, { opacity, transform: [{ scale }] }]}
+            />
+          );
+        })}
+      </View>
     </View>
   );
 };

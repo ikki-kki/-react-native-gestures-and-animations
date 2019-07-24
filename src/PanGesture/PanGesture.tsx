c37bc703f904dc9629ae1d130ac46c84b7eb480e
William Candillon <wcandillon@gmail.com>
2019-07-24 10:24:41 +0200

    ⚙️

--- src/PanGesture/PanGesture.tsx
+++ src/PanGesture/PanGesture.tsx
@@ -1,10 +1,47 @@
 import * as React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Dimensions, Image } from "react-native";
+import { PanGestureHandler, State } from "react-native-gesture-handler";
+import Animated from "react-native-reanimated";
+
+import { onGestureEvent } from "react-native-redash";
+import { cards, StyleGuide } from "../components";
+
+const { Value } = Animated;
+const { width: wWidth } = Dimensions.get("window");
+const width = wWidth - StyleGuide.spacing * 8;
+const CARD_ASPECT_RATIO = 1324 / 863;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: StyleGuide.palette.background,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  card: {
+    width,
+    height: width / CARD_ASPECT_RATIO
+  }
+});
+const [card] = cards;
 
 export default () => {
+  const state = new Value(State.UNDETERMINED);
+  const translationX = new Value(0);
+  const translationY = new Value(0);
+  const gestureHandler = onGestureEvent({ state, translationX, translationY });
+  const translateX = translationX;
+  const translateY = translationY;
   return (
-    <View
-      style={{ ...StyleSheet.absoluteFillObject, backgroundColor: "red" }}
-    />
+    <View style={styles.container}>
+      <PanGestureHandler {...gestureHandler}>
+        <Animated.View
+          style={{
+            transform: [{ translateX }, { translateY }]
+          }}
+        >
+          <Image style={styles.card} source={card.source} />
+        </Animated.View>
+      </PanGestureHandler>
+    </View>
   );
 };

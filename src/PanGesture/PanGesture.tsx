0d505ae63ecd5cc1e3607694fb8def6c33dd1fba
William Candillon <wcandillon@gmail.com>
2019-07-24 10:43:05 +0200

    ⚙️

--- src/PanGesture/PanGesture.tsx
+++ src/PanGesture/PanGesture.tsx
@@ -2,24 +2,26 @@ import * as React from "react";
 import { View, StyleSheet, Dimensions, Image } from "react-native";
 import { PanGestureHandler, State } from "react-native-gesture-handler";
 import Animated from "react-native-reanimated";
+import Constants from "expo-constants";
 
-import { onGestureEvent } from "react-native-redash";
+import { onGestureEvent, decay, clamp } from "react-native-redash";
 import { cards, StyleGuide } from "../components";
 
 const { Value } = Animated;
-const { width: wWidth } = Dimensions.get("window");
-const width = wWidth - StyleGuide.spacing * 8;
+const { width, height } = Dimensions.get("window");
+const containerWidth = width;
+const containerHeight = height - Constants.statusBarHeight - 44;
 const CARD_ASPECT_RATIO = 1324 / 863;
+const cardWidth = width - StyleGuide.spacing * 8;
+const cardHeight = cardWidth / CARD_ASPECT_RATIO;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: StyleGuide.palette.background,
-    justifyContent: "center",
-    alignItems: "center"
+    backgroundColor: StyleGuide.palette.background
   },
   card: {
-    width,
-    height: width / CARD_ASPECT_RATIO
+    width: cardWidth,
+    height: cardHeight
   }
 });
 const [card] = cards;
@@ -28,9 +30,25 @@ export default () => {
   const state = new Value(State.UNDETERMINED);
   const translationX = new Value(0);
   const translationY = new Value(0);
-  const gestureHandler = onGestureEvent({ state, translationX, translationY });
-  const translateX = translationX;
-  const translateY = translationY;
+  const velocityX = new Value(0);
+  const velocityY = new Value(0);
+  const gestureHandler = onGestureEvent({
+    state,
+    translationX,
+    translationY,
+    velocityX,
+    velocityY
+  });
+  const translateX = clamp(
+    decay(translationX, state, velocityX),
+    0,
+    containerWidth - cardWidth
+  );
+  const translateY = clamp(
+    decay(translationY, state, velocityY),
+    0,
+    containerHeight - cardHeight
+  );
   return (
     <View style={styles.container}>
       <PanGestureHandler {...gestureHandler}>

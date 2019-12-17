b2208ca52682f1139ec43806b7c6372b8d8c908f
William Candillon <wcandillon@gmail.com>
2019-12-17 11:30:26 +0100

    💄

--- src/Svg/Svg.tsx
+++ src/Svg/Svg.tsx
@@ -8,20 +8,14 @@ import Animated from "react-native-reanimated";
 import { onGestureEvent } from "react-native-redash";
 import { StyleGuide, withTransition } from "../components";
 
-const { Value, sub, multiply, abs, cond, eq, max } = Animated;
-const AnimatedEllipse = Animated.createAnimatedComponent(Ellipse);
+const { Value, sub, multiply, eq, cond, max, abs } = Animated;
 const { width, height } = Dimensions.get("window");
+const AnimatedEllipse = Animated.createAnimatedComponent(Ellipse);
 const containerWidth = width;
 const containerHeight = height - Constants.statusBarHeight - 44;
 const center = {
   x: containerWidth / 2,
   y: containerHeight / 2
-};
-const canvas2Euclidean = (
-  x: Animated.Adaptable<number>,
-  y: Animated.Adaptable<number>
-) => {
-  return { cx: sub(x, center.x), cy: multiply(sub(y, center.y), -1) };
 };
 const radius = 100;
 const styles = StyleSheet.create({
@@ -30,27 +24,44 @@ const styles = StyleSheet.create({
   }
 });
 
+const canvas2Euclidean = (
+  x: Animated.Node<number>,
+  y: Animated.Node<number>
+) => {
+  return {
+    rx: sub(x, center.x),
+    ry: multiply(sub(y, center.y), -1)
+  };
+};
+
 export default () => {
   const x = new Value(0);
   const y = new Value(0);
   const velocityX = new Value(0);
   const velocityY = new Value(0);
   const state = new Value(State.UNDETERMINED);
-  const gestureHandler = onGestureEvent({ y, x, velocityX, velocityY, state });
+  const gestureHandler = onGestureEvent({
+    x,
+    y,
+    velocityX,
+    velocityY,
+    state
+  });
   const isActive = eq(state, State.ACTIVE);
   const targetX = cond(isActive, x, center.x);
   const targetY = cond(isActive, y, center.y);
-  const x1 = withTransition(targetX, velocityX);
-  const y1 = withTransition(targetY, velocityY);
-  const { cx, cy } = canvas2Euclidean(x1, y1);
+  const { rx, ry } = canvas2Euclidean(
+    withTransition(targetX, velocityX),
+    withTransition(targetY, velocityY)
+  );
   return (
     <View style={styles.container}>
       <Svg style={StyleSheet.absoluteFill}>
         <AnimatedEllipse
           cx={center.x}
           cy={center.y}
-          rx={max(abs(cx), radius)}
-          ry={max(abs(cy), radius)}
+          rx={max(abs(rx), radius)}
+          ry={max(abs(ry), radius)}
           fill={StyleGuide.palette.primary}
         />
       </Svg>

a7bf783d0f2058a8514985b495ef830cd3b720ba
William Candillon <wcandillon@gmail.com>
2020-02-22 14:56:29 +0100

    Values, Clocks, and their identity (#40)

--- src/ClockValuesAndIdentities/ClockValuesAndIdentities.tsx
+++ src/ClockValuesAndIdentities/ClockValuesAndIdentities.tsx
@@ -11,15 +11,12 @@ const {
   Value,
   Clock,
   block,
-  not,
+  eq,
   cond,
-  clockRunning,
   startClock,
   interpolate,
   Extrapolate,
-  add,
-  eq,
-  stopClock
+  add
 } = Animated;
 
 const duration = 2000;
@@ -36,49 +33,53 @@ const styles = StyleSheet.create({
 
 export default () => {
   const [show, setShow] = useState(true);
-  const { time, clock, progress } = useMemoOne(
+  const { from, to, clock, progress } = useMemoOne(
     () => ({
-      time: new Value(0),
       clock: new Clock(),
-      progress: new Value(0)
+      progress: new Value(0),
+      from: new Value(0),
+      to: new Value(0)
     }),
     []
   );
-  const opacity = interpolate(progress, {
-    inputRange: [0, 1],
-    outputRange: show ? [0, 1] : [1, 0]
-  });
+  // The only side effect in our component is setShow
+  // if they where other we would write
+  // time = useMemoOne(() => new Value(-1), [show]);
+  const time = new Value(-1);
   useCode(
     () =>
       block([
         // 1. If the clock is not running
         // start the clock and save the original clock value in
-        cond(not(clockRunning(clock)), [startClock(clock), set(time, clock)]),
+        cond(eq(time, -1), [
+          set(from, progress),
+          set(to, show ? 1 : 0),
+          startClock(clock),
+          set(time, clock)
+        ]),
         // 2. Calculate the progress of the animation
         set(
           progress,
           interpolate(clock, {
             inputRange: [time, add(time, duration)],
-            outputRange: [0, 1],
+            outputRange: [from, to],
             extrapolate: Extrapolate.CLAMP
           })
-        ),
-        // 3. If the animation is over, stop the clock
-        cond(eq(progress, 1), stopClock(clock))
+        )
       ]),
-    [clock, progress, time]
+    [clock, from, progress, show, time, to]
   );
   return (
     <View style={styles.container}>
       <View style={styles.card}>
-        <Animated.View style={{ opacity }}>
+        <Animated.View style={{ opacity: progress }}>
           <Card card={cards[0]} />
         </Animated.View>
       </View>
       <Button
         label={show ? "Hide" : "Show"}
         primary
-        onPress={() => setShow(!show)}
+        onPress={() => setShow(prev => !prev)}
       />
     </View>
   );
